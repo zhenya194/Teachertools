c982fdceb39e7085a295a607f8eb40673841007d
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-14 17:11:01 +0200

    Add tests for avarage_mark func

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -2,3 +2,9 @@ from teachertools.teachertools import average_mark
 
 def test_average_mark():
     assert average_mark(10, 5) == 7.5
+def test_average_mark():
+    assert average_mark(5, 5, 5) == 5
+def test_average_mark():
+    assert average_mark(2) == 2
+def test_average_mark():
+    assert average_mark(3.5, 7, 11, 4.5) == 6.5
