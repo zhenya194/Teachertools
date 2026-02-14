8a0dcb8fef3bad4ebfe44f4bb5ce3e79466e00f7
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-14 15:19:11 +0200

    Initial commit

---
+++ teachertools.py
@@ -0,0 +1,11 @@
+def average_mark(*marks):
+    sum_of_marks:float = 0
+    amount = 0
+    ave_mark:float = 0
+    for i in range(len(marks)):
+        sum_of_marks += marks[i]
+        amount += 1
+    ave_mark = sum_of_marks / amount
+    return ave_mark
+
+print(average_mark(5, 9))
