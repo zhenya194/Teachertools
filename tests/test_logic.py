e900354a8f36e1c6253201ef6ee9aee3c0d0bb15
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-14 16:55:15 +0200

    Fix tests

---
+++ tests/test_logic.py
@@ -0,0 +1,4 @@
+from teachertools.teachertools import average_mark
+
+def test_average_mark():
+    assert average_mark(10, 5) == 7.5
--- tests/tests_logic.py
+++
@@ -1,4 +0,0 @@
-from src.teachertools import average_mark
-
-def check_average_mark():
-    assert average_mark(10, 5) == 7.5
