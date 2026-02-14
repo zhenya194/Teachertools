fb6544c045fea242c76f050a9eca87f61e799b62
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-14 18:52:10 +0200

    Rename variable

--- src/teachertools/teachertools.py
+++ src/teachertools/teachertools.py
@@ -1,9 +1,9 @@
 def average_mark(*marks:list[int | float]):
     sum_of_marks:float = 0
     amount:int = 0
-    ave_mark:float = 0
+    average_mark:float = 0
     for i in range(len(marks)):
         sum_of_marks += marks[i]
         amount += 1
-    ave_mark = sum_of_marks / amount
-    return ave_mark
+    average_mark = sum_of_marks / amount
+    return average_mark
