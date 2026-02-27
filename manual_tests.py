d64c5ca68cb25ee8c49737cee3fe44f727cffcf1
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-27 13:14:46 +0200

    Update manual_tests.py

--- manual_tests.py
+++ manual_tests.py
@@ -3,4 +3,7 @@ from teachertools import *
 # s:list = ["one", "two", "three", "four"]
 # students(*s)
 
-print(is_enter(12, 10, 1, 4, 7, 4, 6))
+# print(is_enter(12, 10, 1, 4, 7, 4, 6))
+
+sc = {"math":"12:00", "it":"14:00"}
+print(schedual(sc))
