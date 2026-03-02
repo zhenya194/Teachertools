f34b16e5ae19f2b203849292f0840490794b6cee
zhenya194 <zhenya.pro1753@gmail.com>
2026-03-02 10:23:43 +0200

    Update manual_tests.py

--- manual_tests.py
+++ manual_tests.py
@@ -5,12 +5,13 @@ from teachertools import *
 
 # print(is_enter(12, 10, 1, 4, 7, 4, 6))
 
-sc:dict = {"math":"12:00", "it":"14:00"}
-cla:list = ["2-g", "10-c"]
+# sc:dict = {"math":"12:00", "it":"14:00"}
+# cla:list = ["2-g", "10-c"]
 # print(schedual_plus(cla, sc))
-schedual_plus(cla, sc)
+# schedual_plus(cla, sc)
 
 # # print(schedual(sc))
 # print(get_schedual_file(sc))
 
-
+# plans:list = ["Learn Python", "Work"]
+# get_plans_file(plans)
